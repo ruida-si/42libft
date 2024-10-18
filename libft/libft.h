cb6e7aaef1af4f5d1c8ef3bbe0c1b95881260149
ruida-si <rui2kk7@gmail.com>
2024-10-18 14:09:32 +0100

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruida-si <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 11:12:55 by ruida-si          #+#    #+#             */
+/*   Updated: 2024/10/18 11:12:59 by ruida-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(char c);
+char	ft_toupper(char c);
+int	ft_isdigit(char c);
+char	ft_tolower(char c);
+int	ft_isalnum(char c);
+
+#endif
